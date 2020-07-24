d722ae348e8ce61ec882b9d8730294a809338702
Maddie Waldie <maddiewaldie@me.com>
2020-07-24 10:13:32 -0700

    added class & functions & assigned people to them

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Tic-Tac-Toe
-A simple Swift Tic-Tac-Toe game
+
+This project is a command line version of tic tac toe. Written in a Swift Xcode Playground, this project is simple and easy to use!
+
+## Installation & Running the Program
+
+**Cloning the Repository:** In order to get the code onto your computer, run the following command in the command line:
+
+```
+$ git clone https://github.com/maddiewaldie/Tic-Tac-Toe.git
+```
+
+**Running the Program:** Follow the instructions below to run the program in Xcode.
+
+1. Launch Xcode (version 10 or higher).
+2. Open `TicTacToe.playground`.
+3. In order to test the code and see how it works, hit the Play button. The playground immediately interprets the code (up to the line of the Play button) and displays the result on the right pane.
+
+## Authors
+
+This Tic-Tac-Toe game was made by Sylvia Chin, Maya Borowicz, Lucinda Quintal, Sydney Schrader, and Madeleine Waldie during the summer of 2020.
--- TicTacToe.playground/Contents.swift
+++ TicTacToe.playground/Contents.swift
@@ -6,4 +6,65 @@
 *  Version: 1.0
 */
 
+struct TicTacToe {
+    var board = [String]()
+    
+    // Maddie - Display intro
+    func displayIntro() -> Void {
+        print("Welcome to TicTacToe!")
+        displayBoard()
+    }
+    
+    // Everyone - Board
+    func displayBoard() -> Void {
+        
+    }
+    
+    // Lucinda - Set up players as x & o
+    func setUpPlayers(name: String) -> Void {
+        // enter name
+        
+    }
 
+    // Sylvia - User Input
+    func getUserInput() -> Void {
+        
+    }
+
+    // Sydney - Check validity of move
+    func checkValid() -> Bool {
+        return true // placeholder
+    }
+
+    // Maya - Switching from x to o (2 player)
+    func switchPlayer() -> Void {
+        
+    }
+
+    // Maddie - Keys - Letters / Numbers show what position
+    func updateBoard() -> Void{
+        
+    }
+
+    // Lucinda - Checking for wins / losses / ties
+    func checkWinLossTie() -> Void {
+        
+    }
+    
+    // Sylvia - Display game over / congrats message
+    func displayGameOver() -> Void {
+        
+    }
+    
+    // Sydney - Ask if players want to play again
+    func playAgain() -> Bool {
+        return true // placeholder
+    }
+    
+    // Maya - Display score
+    func displayScore() -> Void {
+        
+    }
+    
+
+}
