76623f142cbb688826e573d0f04e6410105975ac
lucinda-usc <lquintal@usc.edu>
2020-07-26 13:30:17 -0700

    player structure changed. checkWinLossTie() started. added quotes around Int values in playAgain() and commented out playAgain() function call in displayGameOver() to make code executable.

--- TicTacToe.playground/Contents.swift
+++ TicTacToe.playground/Contents.swift
@@ -9,17 +9,16 @@
 class TicTacToe {
     var board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
 
-    //this block creates and initializes a subclass so that the players have the attributes name and mark
-    class player:TicTacToe {
-        var name:String;
-        var mark:String;
+    var player1:String
+    var player2:String
 
-        init(name:String, mark:String) {
-            self.name = name;
-            self.mark = mark;
-        }
+    //this variable can be used to check across functions to see if the game has ended (changed in checkWinLossTie())
+    var gameOver = false
+
+    init(player1:String, player2:String) {
+        self.player1=player1
+        self.player2=player2
     }
-
 
     // Maddie - Display intro
     func displayIntro() -> Void {
@@ -57,11 +56,12 @@ class TicTacToe {
         }else{
             return false
         }*/
+        return false
     }
 
     // Maya - Switching from x to o (2 player)
     func switchPlayer() -> Void {
-        
+
     }
 
     // Maddie - Update board to indicate where player placed their X or O
@@ -74,30 +74,47 @@ class TicTacToe {
                 }
             }
         }
-
+//        checkWinLossTie(number:location, token:activeMark)
         // Display new board
         print("\n")
         displayBoard()
     }
 
     // Lucinda - Checking for wins / losses / ties
-    func checkWinLossTie() -> Void {
-        
-    }
+//    func checkWinLossTie(location:String, activeMark:String) -> Void {
+//        var activePlayer = String {
+//            if(activeMark=="X") {
+//                return player1
+//            }
+//            return player2
+//        }
+//
+//        if(checkForHorizontalWin()||checkForVerticalWin()||checkForDiagonalWin()) {
+//            print("\(activePlayer) has won as mark \(activeMark)!")
+//            gameOver = true;
+//        } else if (boardIsFull()) {
+//            print("This game is a tie!")
+//        }
+//
+//        //TODO: make functions checkForHorizontalWin(), checkForVerticalWin(), checkForDiagonalWin(), and boardIsFull()
+//        //above all have return type Bool
+//
+//    }
+
     
     // Sylvia - Display game over / congrats message
     func displayGameOver() -> Void {
         print ("Game Over!")
-        playAgain()
+//        playAgain()
     }
     
     // Sydney - Ask if players want to play again
     func playAgain() -> Bool {
         print("Would you like to play again? 1 for yes 2 for no")
         let answer = readLine()
-        if answer == 1{
+        if answer == "1"{
             return true
-        }else if answer == 2{
+        }else if answer == "2"{
             return false
         }else{
             return false
@@ -117,20 +134,16 @@ class TicTacToe {
 //----------------------------------------------
 
 
-//creates an instance of the game TicTacToe
-var tictactoe = TicTacToe()
-
 //players input their name which is stored in the temporary variables p1name and p2name
-print("Enter Player 1 name: ")
+print("Enter Player 1 (X) name: ")
 let p1name = readLine()!
-print("Enter Player 2 name: ")
+print("Enter Player 2 (O) name: ")
 let p2name = readLine()!
 
-//creates two player objects, with the properties that were just inputted
-var player1 = TicTacToe.player(name:p1name, mark:"X")
-var player2 = TicTacToe.player(name:p2name, mark:"Y")
+//creates an instance of the game TicTacToe with both players' names
+var tictactoe = TicTacToe(player1:p1name, player2:p2name)
 
 tictactoe.displayIntro()
-print("Player 1, \(player1.name) has mark \(player1.mark)")
-print("Player 2, \(player2.name) has mark \(player2.mark)")
+print("Player 1, \(tictactoe.player1) has mark X")
+print("Player 2, \(tictactoe.player2) has mark O")
 //tictactoe.updateBoard()
